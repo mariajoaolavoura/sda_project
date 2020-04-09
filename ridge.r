f5311cdfc7e2297459065a3328d54ced1887e816
NunonuN@github.com <nunogomes.pt@gmail.com>
2020-04-09 18:32:24 +0100

    Add lda.r; add ridge.r; add figures

---
+++ ridge.r
@@ -0,0 +1,83 @@
+###
+## Ridge Regression
+###
+
+## libraries
+require(glmnet)
+require(FSA)
+
+## seed
+set.seed(123)
+
+## functions
+
+#' Function \code{train_test_split}
+#'
+#' Split data set into train and test sets
+#' @author Nuno R. C. Gomes <nunorcgomes@@gmail.com> (2020/03/18)
+#' @param dataset A data set.
+#' @param target Target variable (name or index).
+#' @param frac Proportion of data set for training.
+#' @param seed A seed for the sampling; Default: 42
+#' @return A vector \code{tts} of mode "list".
+#' @examples
+#' train_test_split(pid, "diabetes", 0.8)
+train_test_split= function(dataset, target, frac, seed= 42) {
+  set.seed(seed)
+  nr= nrow(dataset)
+  seqnr= 1:nr
+  # target variable and index
+  if (typeof(target) == "character") {
+    target.idx= which(colnames(dataset) == target)
+  } else if (typeof(target) == "double") {
+    target.idx= target
+    target= colnames(dataset[target.idx])
+  } else {
+    stop("Invalid `target` argument.")
+  }
+  
+  # train and test sets
+  train.idx= sample(seqnr, frac*nr)
+  test.idx=  setdiff(seqnr, train.idx)
+  X.train=   data.set[train.idx, -target.idx]
+  y.train=   data.set[train.idx,  target.idx]
+  X.test=    data.set[test.idx,  -target.idx]
+  y.test=    data.set[test.idx,   target.idx]
+  
+  # list of data frames
+  tts= vector('list', 4)
+  names(tts)= c('X_train', 'y_train', 'X_test', 'y_test')
+  tts[[1]]= X.train
+  tts[[2]]= y.train
+  tts[[3]]= X.test
+  tts[[4]]= y.test
+  
+  return(tts)
+}
+
+## read data
+data.set= read.csv("./data/cardio_data.csv")
+headtail(data.set)
+
+## list of categorical/binary variables
+## gender, choles, glucose, smoke, alcohol, active, cardio
+
+## factorise categorical/binary variables
+colNames.cat= c('gender', 'choles', 'glucose', 'smoke',
+                'alcohol', 'active', 'cardio')
+data.set[, colNames.cat]= lapply(data.set[, colNames.cat], factor)
+
+## train/test split
+tts= train_test_split(data.set, "cardio", 0.7, seed= 123)
+x.train= tts$X_train
+y.train= tts$y_train
+x.test=  tts$X_test
+y.test=  tts$y_test
+
+ridge.train= model.matrix(y.train ~ ., data= x.train)[, -1] # remove intercept
+ridge.test=  model.matrix(y.test ~ ., data= x.test)[, -1]
+
+# optimal values for lambda (10-fold cross-validation)
+alpha0.fit= cv.glmnet(ridge.train, y.train,
+                      type.measure= "deviance", alpha= 0, family= "binomial")
+alpha0.predict= predict(alpha0.fit, s= alpha0.fit$lambda.1se, newx= )
